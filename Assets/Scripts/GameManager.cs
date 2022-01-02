b1d0d8f94f331be6d74eac57593568bde27f5fb1
boothemjr <maurice.boothe.jr@gmail.com>
2022-01-02 11:45:35 -0600

    add piece placement from file

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq.Expressions;
 using UnityEngine;
 using Utilities;
 using Debug = UnityEngine.Debug;
@@ -18,7 +19,7 @@ public class GameManager : MonoBehaviour
     const string FILE_DEFAULT_SAVE = "default";
     private string DATA_PATH;
     private string FILE_SAVE_PATH;
-    
+
     public GameObject squareB;
     public GameObject squareW;
 
@@ -100,11 +101,45 @@ public class GameManager : MonoBehaviour
     
     private void AddPieces()
     {
+        System.IO.StringReader sr = new StringReader(gameSaveData);
+        
+        string[,] pieceLocations = new string[boardWidth, boardHeight];
+        for (int x = 0; x < boardWidth; x++)
+        {
+            string nextLine = sr.ReadLine();
+            for (int y = 0; y < boardHeight; y++)
+            {
+                pieceLocations[x, y] = nextLine.Substring(0, 2);
+                nextLine = nextLine.Substring(2);
+                Debug.Log(pieceLocations[x,y] + " added at location " + x + "," + y);
+            }
+        }
+
         for (int x = 0; x < boardWidth; x++)
         {
             for (int y = 0; y < boardHeight; y++)
             {
-                var piece = Instantiate(pawnW);
+                GameObject piece = new GameObject();
+                string flag = pieceLocations[x, y];
+                
+                switch (flag)
+                {
+                    case "rb" : piece = Instantiate(rookB); break;
+                    case "nb" : piece = Instantiate(knightB); break;
+                    case "bb" : piece = Instantiate(bishopB); break;
+                    case "qb" : piece = Instantiate(queenB); break;
+                    case "kb" : piece = Instantiate(kingB); break;
+                    case "pb" : piece = Instantiate(pawnB); break;
+                    case "rw" : piece = Instantiate(rookW); break;
+                    case "nw" : piece = Instantiate(knightW); break;
+                    case "bw" : piece = Instantiate(bishopW); break;
+                    case "qw" : piece = Instantiate(queenW); break;
+                    case "kw" : piece = Instantiate(kingW); break;
+                    case "pw" : piece = Instantiate(pawnW); break;
+                    default : break;
+
+                }
+                
                 piece.transform.localScale = new Vector3(.5f, .5f, .5f);
                 piece.transform.position = new Vector3(x - camOffSet, .5f, y - camOffSet);
             }
